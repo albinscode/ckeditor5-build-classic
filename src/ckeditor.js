6ef0f9f96dd8686d0f0a955dfb4c41cabc5458b8
Albin VIGIER <avigier@linagora.com>
2022-02-24 18:36:27 +0100

    with source editing

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -31,6 +31,8 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -58,13 +60,16 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+
+	SourceEditing,
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
+			'sourceEditing',
 			'heading',
 			'|',
 			'bold',
